a94b2f4328e07d2437a0b2c4ddd1e8ec9689cb85
alexandresnevesjr-byte <alexandres.nevesjr@gmail.com>
2026-09-16 23:33:05 -0300

    feat: funcao salvar audio e caminho

--- audio/recorder.py
+++ audio/recorder.py
@@ -1,1 +1,16 @@
 #arquivo para tentar implementar gravador de áudio
+
+from pathlib import Path
+
+
+def save_audio(audio_data: bytes, filepath: str | Path) -> Path:
+    filepath = Path(filepath)
+
+    filepath.parent.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    filepath.write_bytes(audio_data)
+
+    return filepath
