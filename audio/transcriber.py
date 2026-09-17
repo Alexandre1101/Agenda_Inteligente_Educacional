d823a771b15a292add04f013e64fbf9c4b97b673
alexandresnevesjr-byte <alexandres.nevesjr@gmail.com>
2026-09-16 22:24:07 -0300

    refactor: arquivo transcriber recebendo a funcão do arquivo mvp.py

--- audio/transcriber.py
+++ audio/transcriber.py
@@ -1,1 +1,13 @@
 #arquivo para deixar funções de transcrição de áudio para texto
+
+import whisper
+
+def transcriber(audio):
+    model = whisper.load_model("large")
+    result = model.transcribe(
+        audio,
+        language="pt",
+        fp16=False
+    )
+    texto = result["text"]
+    return texto
